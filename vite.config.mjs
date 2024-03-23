b79bc067dcdda06b3c42f0014821d3f51d257ef3
Alexandre St-Laurent <alexandre.stlaurent.4@gmail.com>
2024-03-23 18:57:38 -0400

    react csr setup

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,6 +1,8 @@
+import viteReact from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+    plugins: [viteReact()],
     publicDir: 'resources/assets/public',
     build: {
         copyPublicDir: true,
